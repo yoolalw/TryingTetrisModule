86497dbbece97127dd2cce7d62c41a978e08965d
yoolalw <larakemilyprofissional@gmail.com>
2026-05-21 16:55:29 -0300

    startando tetris

---
+++ modA_NotOfficial/js/tetris.js
@@ -0,0 +1,9 @@
+console.log("aaaa")
+
+const tetrisContainer = document.querySelector('#tetris');
+const context = tetrisContainer.getContext('2d');
+context.fillStyle = "rgb(198, 168, 255)"
+context.fillRect(0, 0, 100, 60) 
+
+context.fillStyle = "rgb(70, 15, 174)"
+context.fillRect(0, 30, 10, 60) 
