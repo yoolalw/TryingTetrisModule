47eee86c87d2659a85cc53ca9b3c3e4c34dbfb77
yoolalw <larakemilyprofissional@gmail.com>
2026-05-22 10:13:43 -0300

    circles with texts

--- modA_NotOfficial/js/tetris.js
+++ modA_NotOfficial/js/tetris.js
@@ -3,14 +3,14 @@ console.log("aaaa")
 let tetrisContainer = document.querySelector('#tetris');
 let context = tetrisContainer.getContext('2d');
 
-var windowHeigh = window.innerHeight
+var windowHeight = window.innerHeight
 var windowWidth = window.innerWidth
 
 tetrisContainer.width = windowWidth
 
 context.fillStyle = "#fff"
 
-context.fillRect(0, 0, 100, 200)
+context.fillRect(0, 0, 100, 400)
 
 context.beginPath();
 context.strokeStyle = "red"
@@ -20,21 +20,33 @@ context.stroke()
 context.closePath()
 
 class Circle {
-    constructor(xpos, ypos, radius, color) {
+    constructor(xpos, ypos, radius, color, text) {
         this.xpos = xpos
         this.ypos = ypos
         this.radius = radius
         this.color = color
+        this.text = text
+    
     }
+
 
     draw(context) {
         context.beginPath();
+        
+        context.fillText(this.text, this.xpos, this.ypos)
+        context.textAlign = "center"
+        context.textBaseline = "middle"
+        context.font = "20px Arial"
+
         context.lineWidth = 4
         context.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false)
         context.stroke()
         context.closePath()
     }
+
 }
+
+let circleCounter = 1;
 
 let allCircles = []
 
@@ -43,10 +55,11 @@ let createCircles = function(circle){
 }
 
 for (var numbers = 0; numbers < 10; numbers++) {
-    let randomX = Math.random() * windowWidth
-    let randomY = Math.random() * windowHeigh
-    let myCircle = new Circle(randomX, randomY, 50, "black")
+    let randomX = Math.random() * windowHeight
+    let randomY = Math.random() * windowWidth
+    let myCircle = new Circle(randomX, randomY, 50, "black", circleCounter)
     allCircles.push(myCircle);
     createCircles(allCircles[numbers]);
+    circleCounter++;
 }
 
