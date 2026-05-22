735c2a32cfee86f8b00c6233cf14622a787723e9
yoolalw <larakemilyprofissional@gmail.com>
2026-05-22 10:50:08 -0300

    moviment in circle

--- modA_NotOfficial/js/tetris.js
+++ modA_NotOfficial/js/tetris.js
@@ -7,10 +7,12 @@ var windowHeight = window.innerHeight
 var windowWidth = window.innerWidth
 
 tetrisContainer.width = windowWidth
+tetrisContainer.height = windowHeight
+
 
 context.fillStyle = "#fff"
 
-context.fillRect(0, 0, 100, 400)
+context.fillRect(0, 0, 800, 500)
 
 context.beginPath();
 context.strokeStyle = "red"
@@ -20,19 +22,22 @@ context.stroke()
 context.closePath()
 
 class Circle {
-    constructor(xpos, ypos, radius, color, text) {
+    constructor(xpos, ypos, radius, color, text, speed) {
         this.xpos = xpos
         this.ypos = ypos
         this.radius = radius
         this.color = color
         this.text = text
-    
+        this.speed = speed
+
+        this.dx = 1 * this.speed
+        this.dy = 1 * this.speed
     }
 
 
     draw(context) {
         context.beginPath();
-        
+
         context.fillText(this.text, this.xpos, this.ypos)
         context.textAlign = "center"
         context.textBaseline = "middle"
@@ -44,22 +49,60 @@ class Circle {
         context.closePath()
     }
 
+    update() {
+        context.clearRect(0, 0, windowWidth, windowHeight)
+        this.draw(context);
+
+        if((this.xpos + this.radius) > windowWidth){
+            this.dx = -this.dx;
+        }
+        if((this.xpos - this.radius) < 0){
+            this.dx = -this.dx;
+        }
+
+        if((this.ypos - this.radius) < 0){
+            this.dy = -this.dy;
+        }
+
+        if((this.ypos + this.radius) > windowHeight){
+            this.dy = -this.dy
+        }
+
+        this.xpos += this.dx;
+        this.ypos += this.dy;
+    }
+
 }
 
 let circleCounter = 1;
 
 let allCircles = []
 
-let createCircles = function(circle){
-    circle.draw(context)
+let randomX = Math.random() * windowHeight
+let randomY = Math.random() * windowWidth
+let myCircle = new Circle(randomX, randomY, 50, "black", circleCounter, 6)
+
+myCircle.draw(context);
+
+let updateCircle = function(){    
+    
+    context.clearRect(0, 0, windowWidth, windowHeight)
+    requestAnimationFrame(updateCircle);
+
+    myCircle.update();
+
 }
 
+updateCircle()
+
+/*
 for (var numbers = 0; numbers < 10; numbers++) {
     let randomX = Math.random() * windowHeight
     let randomY = Math.random() * windowWidth
-    let myCircle = new Circle(randomX, randomY, 50, "black", circleCounter)
+    let myCircle = new Circle(randomX, randomY, 50, "black", circleCounter, 1)
     allCircles.push(myCircle);
     createCircles(allCircles[numbers]);
     circleCounter++;
 }
 
+*/
