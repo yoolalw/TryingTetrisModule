03d7dd8c8136e7604fd7686471085dd1931e3f39
yoolalw <larakemilyprofissional@gmail.com>
2026-05-25 16:30:17 -0300

    criando peca1 ++ tentando fazer ela animar

--- modA_NotOfficial/js/tetrisPage.js
+++ modA_NotOfficial/js/tetrisPage.js
@@ -16,14 +16,14 @@ const intervalo = setInterval(() => {
 let myCanvas = document.querySelector("#canvas");
 let context = myCanvas.getContext("2d");
 
-function drawGrid(lineWidth, cellWidth, cellHeight, color) {
+function drawGrid(lineWidth, cellWidth, cellHeight, color, speed) {
     context.strokeStyle = color;
     context.lineWidth = lineWidth;
 
     let width = myCanvas.width;
     let height = myCanvas.height;
 
-    
+
     for (let x = 0; x <= width; x += cellWidth) {
         context.beginPath()
         context.moveTo(x, 0);
@@ -37,12 +37,21 @@ function drawGrid(lineWidth, cellWidth, cellHeight, color) {
         }
     }
 
-    let pecas = [
-        {id: "1", width: cellWidth, height: cellHeight, color: "red", text: "I"},
-        {id: "2", width: cellWidth, height: cellHeight, color: "blue", text: "O"},
-        {id: "3", width: cellWidth, height: cellHeight, color: "green", text: "T"},
-    ]
-    
+    let peca1 = [
+        { id: "1", width: cellWidth, height: cellHeight, color: "blue", speed: 1 }
+    ];
+    for (let i = 0; i < peca1.length; i++) {
+        let peca = peca1[i]
+        context.beginPath()
+        context.fillStyle = peca.color
+        context.fillRect(cellWidth, cellHeight, peca.width * 3, peca.height)
+        context.fillRect(cellWidth * 3, i * cellHeight, peca.width, peca.height)
+        dx = 1 * peca.speed
+        context.stroke()
+    }
+
 }
+
+
 drawGrid(2, 80, 60, "black")
 
