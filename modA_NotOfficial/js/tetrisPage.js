7468b619f9208a04f4fcb5be99bf42c7099c61ec
yoolalw <larakemilyprofissional@gmail.com>
2026-05-25 15:17:09 -0300

    grid 6x20

--- modA_NotOfficial/js/tetrisPage.js
+++ modA_NotOfficial/js/tetrisPage.js
@@ -7,8 +7,42 @@ const intervalo = setInterval(() => {
     if (tempoRest > 0) {
         contReg.textContent = tempoRest
     }
-    else if(tempoRest == 0){
+    else if (tempoRest == 0) {
         contReg.textContent = ""
     }
 
 }, 1000)
+
+let myCanvas = document.querySelector("#canvas");
+let context = myCanvas.getContext("2d");
+
+function drawGrid(lineWidth, cellWidth, cellHeight, color) {
+    context.strokeStyle = color;
+    context.lineWidth = lineWidth;
+
+    let width = myCanvas.width;
+    let height = myCanvas.height;
+
+    
+    for (let x = 0; x <= width; x += cellWidth) {
+        context.beginPath()
+        context.moveTo(x, 0);
+        context.lineTo(x, height);
+        context.stroke()
+        for (let y = 0; y <= height; y += cellHeight) {
+            context.beginPath()
+            context.moveTo(0, y)
+            context.lineTo(width, y)
+            context.stroke()
+        }
+    }
+
+    let pecas = [
+        {id: "1", width: cellWidth, height: cellHeight, color: "red", text: "I"},
+        {id: "2", width: cellWidth, height: cellHeight, color: "blue", text: "O"},
+        {id: "3", width: cellWidth, height: cellHeight, color: "green", text: "T"},
+    ]
+    
+}
+drawGrid(2, 80, 60, "black")
+
