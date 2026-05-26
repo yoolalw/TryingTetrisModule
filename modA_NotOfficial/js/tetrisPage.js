42187bb672060d5121e10aed810b492273f03b5f
yoolalw <larakemilyprofissional@gmail.com>
2026-05-26 13:23:47 -0300

    sem spawn sozinho

--- modA_NotOfficial/js/tetrisPage.js
+++ modA_NotOfficial/js/tetrisPage.js
@@ -16,36 +16,35 @@ const intervalo = setInterval(() => {
 }, 1000)
 
 
+let myCanvas = document.querySelector("#canvas");
+let context = myCanvas.getContext("2d");
 
-function tetris() {
+function drawGrid(lineWidth, cellWidth, cellHeight, color) {
+    context.strokeStyle = color;
+    context.lineWidth = lineWidth;
 
-    let myCanvas = document.querySelector("#canvas");
-    let context = myCanvas.getContext("2d");
-
-    function drawGrid(lineWidth, cellWidth, cellHeight, color) {
-        context.strokeStyle = color;
-        context.lineWidth = lineWidth;
-
-        let width = myCanvas.width;
-        let height = myCanvas.height;
+    let width = myCanvas.width;
+    let height = myCanvas.height;
 
 
-        for (let x = 0; x <= width; x += cellWidth) {
+    for (let x = 0; x <= width; x += cellWidth) {
+        context.beginPath()
+        context.moveTo(x, 0);
+        context.lineTo(x, height);
+        context.stroke()
+
+        for (let y = 0; y <= height; y += cellHeight) {
             context.beginPath()
-            context.moveTo(x, 0);
-            context.lineTo(x, height);
+            context.moveTo(0, y)
+            context.lineTo(width, y)
             context.stroke()
 
-            for (let y = 0; y <= height; y += cellHeight) {
-                context.beginPath()
-                context.moveTo(0, y)
-                context.lineTo(width, y)
-                context.stroke()
-
-            }
         }
     }
+}
+drawGrid(2, 80, 60, "#333333")
 
+function tetris() {
 
     const cellW = 60
     const cellH = 80
@@ -57,36 +56,71 @@ function tetris() {
 
     }
 
-    const peca1 = {
-        x: 0,
-        y: 60,
-        width: cellH * 3,
-        height: cellW,
-        speed: 2,
-        color: "#A2DED0"
-    }
+    const pecas = [
+        {
+            x: 0,
+            y: 0,
+            width: cellH * 3,
+            height: cellW,
+            speed: 0.5,
+            color: "#A2DED0"
+        },
+        {
+            x: 100,
+            y: 0,
+            width: cellH,
+            height: cellW * 3,
+            speed: 0.5,
+            color: "#333"
+        }
+    ]
 
-    const peca1_top = {
-        x: 160,
-        y: 0,
-        width: cellH,
-        height: cellW,
-        speed: 2,
-        color: "#A2DED0"
-    }
+
+    document.addEventListener("keydown", function (e) {
+        pecas.forEach(p => {
+
+            if (e.key === 'w') {
+                p.speed += 0.1
+            } else if
+                (e.key === 'a') {
+                if (p.x > 0) p.x -= p.speed
+
+            } else if
+                (e.key === 'd') {
+                if (p.x + p.width < myCanvas.width) p.x += p.speed
+            }
+        })
+    })
+
+    function spawnPeca() {
+        const tipo = pecas[Math.floor(Math.random() * pecas.length)]
+        return { 
+            x: Math.floor(Math.random() * myCanvas.width - tipo.width),
+            y:0, 
+            width: tipo.width,
+            height: tipo.height,
+            speed: 0.5,
+            color: tipo.color
+        }
+    } 
+    const spawn = [spawnPeca()]
 
     console.log("a")
     function anim() {
         context.clearRect(0, 0, myCanvas.width, myCanvas.height);
         context.fillStyle = "blue"
 
+
         drawGrid(2, 80, 60, "#333333")
-        drawPeca(peca1.x, peca1.y, peca1.width, peca1.height, peca1.color)
-        drawPeca(peca1_top.x, peca1_top.y, peca1_top.width, peca1_top.height, peca1_top.color)
 
+        pecas.forEach((p) => {
+            drawPeca(p.x, p.y, p.width, p.height, p.color)
+            if (p.y + p.height < myCanvas.height) { 
+                p.y += p.speed 
+            } else {
+                pecas[i] = spawnPeca() }   
 
-        if (peca1.y + peca1.height < myCanvas.height) peca1.y += peca1.speed
-        if (peca1_top.y + peca1_top.height < peca1.y) peca1_top.y += peca1_top.speed
+        })
 
         requestAnimationFrame(anim)
     }
