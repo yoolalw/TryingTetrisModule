8866964c5ca3963eee9dd4aac76da86817fb9eb1
yoolalw <larakemilyprofissional@gmail.com>
2026-05-27 16:06:01 -0300

    com base

--- modA_NotOfficial/js/tetrisPage.js
+++ modA_NotOfficial/js/tetrisPage.js
@@ -1,5 +1,7 @@
 //cria um bloco especifico com base nas coordenadas dele
 
+
+
 class Block {
     constructor(x, y, color) {
         this.x = x
@@ -9,7 +11,7 @@ class Block {
 
     draw() {
         push()
-        let s = width / 12
+        let s = width / 6
         rect(s * this.x, s * this.y, s, s)
         pop()
     }
@@ -35,9 +37,9 @@ class Mov {
             case 1:
                 blocks = [new Block(-1, 0), new Block(0, 0), new Block(0, -1), new Block(1, -1)]
                 break;
-
-            case 2:
-                blocks = [new Block(0, 1), new Block(1, 1), new Block(0, 1), new Block(0, -1)];
+                      
+            case 2:     
+                blocks = [new Block(0, 1), new Block(1, 0), new Block(0, 1), new Block(1, 1), new Block(-1, 1)];
                 break;
         }
 
@@ -61,7 +63,7 @@ class Mov {
 
     //recria varias peças.
     copy() {
-        return new Mov(this.x, this.y, this.color, this.shape);
+        return new Mov(this.x, this.y, this.shape);
     }
 }
 
@@ -69,16 +71,16 @@ class Field {
 
     constructor() {
         this.tiles = [
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1],
             [1, 1, 1, 1, 1, 1]
         ]
     }
@@ -86,7 +88,7 @@ class Field {
 
     //faz a verificacao onde se x e y forem menor, vai retornar 1 a cada mov
     tileAt(x, y) {
-        if (x < 0 || x >= 6 || y < 0 || y >= 10) return 1;
+        if (x < 0 || x >= 6 || y < 0 || y >= 11) return 1;
         return this.tiles[y][x];
     }
     //fixa o bloco no campo
@@ -107,7 +109,7 @@ class Field {
 
     cutLine(y) {
         this.tiles.splice(y, 1);
-        this.tiles.unshift([0, 0, 0, 0, 0, 0]);
+        this.tiles.unshift([1, 0, 0, 0, 0, 1]);
     }
     draw() {
         for (let y = 0; y < 10; y++) {
@@ -121,16 +123,83 @@ class Field {
 }
 class Game {
     constructor() {
-        this.mino = Game.makeMino()
-        this.minoVx = 0;
-        this.minoDrop = false;
-        this.minoVr = 0;
+        this.mov = Game.makeMov()
+        this.movVx = 0;
+        this.movDrop = false;
+        this.movV = 0;
         this.field = new Field()
         this.fc = 0;
     }
-    static makeMino(){
-        return new Mino(5,2,0, floor(random(0,1)))
+    static makeMov() { //retorna uma nova atualizacao de lugar e gera aleatoriamente uma das 2 pecas
+        return new Mov(2, 2, floor(random(1, 3)))
     }
+
+    // pega os blocos de acordo com o mov e retorna a atualizacao de acordo com a funcao de verificacao, onde ele sempre vai criar onde é 0
+    static isMovMovable(mov, field) {
+        let blocks = mov.creatBlocks();
+        return blocks.every(b => field.tileAt(b.x, b.y) === 0);
+    }
+
+    proc() {
+        const tryMove = (dx = 0, dy = 0) => {
+            const f = this.mov.copy() // cria uma copia atual da peca
+
+            f.x += dx;
+            f.y += dy;
+            if (Game.isMovMovable(f, this.field)) { // verifica se a copia ta numa posicao valida 
+                this.mov.x += dx; // se for valida, modifique
+                this.mov.y += dy;
+                return true //movimento funcionand
+            }
+            return false
+        }
+
+        if (this.movDrop || (this.fc % 20) === 19) {
+            if (!tryMove(0, 1)) {
+                //fixa cada bloco no campo
+                for (let b of this.mov.creatBlocks())
+                    this.field.putBlock(b.x, b.y);
+                //cria nova peca aleatoria 
+                this.mov = Game.makeMov()
+            }
+            //remove todas as linhas cheias do field
+            let line;
+            while ((line = this.field.findLineFilled()) !== -1)
+                this.field.cutLine(line);
+            //reseta o flag do drop
+            this.movDrop = false
+        }
+
+        if (this.movVx !== 0) {
+            tryMove(this.movVx);
+            this.movVx = 0;
+        }
+
+        background(64)
+
+        this.mov.draw()
+        this.field.draw()
+
+        this.fc++;
+
+    
+    }
+
+}
+let game;
+
+function keyPressed(){
+    if(keyCode === 65) game.movVx = -1
+    if(keyCode === 68) game.movVx = 1
+    if(keyCode === 83) game.movDrop = true
+}
+
+function setup(){
+    createCanvas(300, 550);
+    game = new Game()
+}
+function draw(){
+    game.proc()
 }
 
 
